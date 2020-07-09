a9ceabefa579a81f6d80f4aee25d8f44e1849319
Stephen van Beek <Stephen van Beek>
2020-07-09 15:04:22 +0100

    Bundled into a yeoman generator

---
+++ README.md
@@ -0,0 +1,36 @@
+# generator-happy-valley-static [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url]
+> A static site generator for our eleventy and parcel setup.
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-happy-valley-static using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-happy-valley-static
+```
+
+Then generate your new project:
+
+```bash
+yo happy-valley-static
+```
+
+## Getting To Know Yeoman
+
+ * Yeoman has a heart of gold.
+ * Yeoman is a person with feelings and opinions, but is very easy to work with.
+ * Yeoman can be too opinionated at times but is easily convinced not to be.
+ * Feel free to [learn more about Yeoman](http://yeoman.io/).
+
+## License
+
+MIT © [Happy Valley IO](https://www.happyvalley.io)
+
+
+[npm-image]: https://badge.fury.io/js/generator-happy-valley-static.svg
+[npm-url]: https://npmjs.org/package/generator-happy-valley-static
+[travis-image]: https://travis-ci.com/HappyValleyIO/generator-happy-valley-static.svg?branch=master
+[travis-url]: https://travis-ci.com/HappyValleyIO/generator-happy-valley-static
+[daviddm-image]: https://david-dm.org/HappyValleyIO/generator-happy-valley-static.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/HappyValleyIO/generator-happy-valley-static
---
+++ generators/app/index.js
@@ -0,0 +1,75 @@
+var Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {
+  async prompting() {
+    this.answers = await this.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Your project name",
+        default: this.appname // Default to current folder name
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "What's the description for this project?"
+      },
+      {
+        type: "input",
+        name: "repository",
+        message: "Repository url:"
+      },
+      {
+        type: "input",
+        name: "author",
+        message: "Who's the author of this project?",
+        default: "Happy Valley IO <support@happyvalley.io>"
+      },
+      {
+        type: "input",
+        name: "license",
+        message: "What's the project license?",
+        default: "MIT"
+      }
+    ]);
+  }
+
+  writing() {
+    this.destinationRoot(`${this.destinationPath(this.answers.name)}`)
+
+    this.fs.copy(
+      this.templatePath(),
+      this.destinationPath()
+    )
+
+    this.fs.copy(
+      this.templatePath(),
+      this.destinationPath(),
+      { globOptions: { dot: true } }
+    )
+
+    this.fs.copyTpl(
+      this.templatePath('package.json'),
+      this.destinationPath('package.json'), {
+        name: this.answers.name,
+        description: this.answers.description,
+        repository: this.answers.repository,
+        author: this.answers.author,
+        license: this.answers.license
+      }
+    )
+  }
+
+  installDeps() {
+    this.log('Installing dependencies')
+    this.destinationRoot(`${this.destinationPath(this.answers.name)}`)
+
+    this.spawnCommand('git', ['init'], {
+      cwd: this.destinationPath()
+    })
+
+    this.spawnCommand('npm', ['ci'], {
+      cwd: this.destinationPath()
+    })
+  }
+};
